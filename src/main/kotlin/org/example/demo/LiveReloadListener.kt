a97af50a97411a7231f29e031e00326c040bf256
Tudor <pagu.tudor@gmail.com>
2023-12-25 17:22:10 +0200

    Live reload works

--- src/main/kotlin/org/example/demo/LiveReloadListener.kt
+++ src/main/kotlin/org/example/demo/LiveReloadListener.kt
@@ -11,46 +11,22 @@ import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.VirtualFileEvent
 import com.intellij.openapi.vfs.VirtualFileListener
 import com.intellij.openapi.vfs.VirtualFilePropertyEvent
+import com.intellij.openapi.vfs.newvfs.BulkFileListener
+import com.intellij.openapi.vfs.newvfs.events.VFileEvent
 import kotlinx.serialization.json.Json
 
 
-class LiveReloadListener: FileDocumentManagerListener {
-    override fun fileContentLoaded(file: VirtualFile, document: Document) {
-        super.fileContentLoaded(file, document)
-    }
+class LiveReloadListener: BulkFileListener {
 
-    override fun afterDocumentUnbound(file: VirtualFile, document: Document) {
-        super.afterDocumentUnbound(file, document)
-    }
-
-    override fun fileContentReloaded(file: VirtualFile, document: Document) {
-        super.fileContentReloaded(file, document)
-    }
-
-
-
-    override fun beforeDocumentSaving(document: Document) {
-        // This method is called before a document is saved
-        // You can perform your live reload action here
-        val virtualFile = FileDocumentManager.getInstance().getFile(document)
-        if (virtualFile != null) {
-            // Check if the file is of interest (optional)
-            if (isFileOfInterest(virtualFile)) {
-                performLiveReloadAction(virtualFile)
+    override fun after(events: MutableList<out VFileEvent>) {
+        for (event in events) {
+            val file = event.file
+            if (file?.name == ".idea_settings.json") {
+                val contentBytes = file.contentsToByteArray()
+                val contentJson = String(contentBytes, Charsets.US_ASCII)
+                val content = Json.parseToJsonElement(contentJson)
+                applySettings(content)
             }
         }
-    }
-
-    private fun isFileOfInterest(virtualFile: VirtualFile): Boolean {
-        // Implement your logic to check if the file is of interest for live reload
-        // For example, check file extension, location, etc.
-        return virtualFile.name == ".idea_settings.json"
-    }
-
-    private fun performLiveReloadAction(virtualFile: VirtualFile) {
-        val contentBytes = virtualFile.contentsToByteArray()
-        val contentJson = String(contentBytes, Charsets.US_ASCII)
-        val content = Json.parseToJsonElement(contentJson)
-        applySettings(content)
     }
 }
